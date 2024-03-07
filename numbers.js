c98e55df139ba3c531d582cb93371751287fae7c
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-03-07 13:48:22 +1300

    experimenting with variables

---
+++ numbers.js
@@ -0,0 +1,9 @@
+// NOTE - const is a constant variable - its value cannot be changed later on in the code
+const num1 = 10;
+console.log(num1);
+
+let num2 = 20;
+console.log(num2);
+
+var num3 = 30;
+console.log(num3);
