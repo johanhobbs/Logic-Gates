1c760cc8df8195e467c661fdac89515003a039b7
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-03-07 14:27:16 +1300

    Numbers tutorial part 1 complete

--- numbers.js
+++ numbers.js
@@ -1,8 +1,16 @@
 // NOTE - const is a constant variable - its value cannot be changed later on in the code
+/*
+
+*/
+const pi = 3.14;
+//pi = 10
+console.log(pi);
+
 const num1 = 10;
 console.log(num1);
 
 let num2 = 20;
+num2 = 25;
 console.log(num2);
 
 var num3 = 30;
