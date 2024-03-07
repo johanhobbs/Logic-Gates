40119f432af27e2aafcb8ed80f14fbcf391dacac
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-03-07 14:42:33 +1300

    strings, concatenation and string templates

--- numbers.js
+++ numbers.js
@@ -15,3 +15,13 @@ console.log(num2);
 
 var num3 = 30;
 console.log(num3);
+
+const fullName = "Johan Hobbs";
+console.log(fullName);
+
+const studyPlace = "NMIT";
+console.log(studyPlace);
+
+console.log(fullName + " " + studyPlace);
+
+console.log(`My full name is ${fullName} and I study at ${studyPlace}`);
